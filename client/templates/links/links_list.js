60a94f5bbb25079af0e05269da7d43df19961688
alexharris <alexharris6@gmail.com>
2014-11-22 13:05:22 -0800

    clean up tag filtering code

--- client/templates/links/links_list.js
+++ client/templates/links/links_list.js
@@ -2,10 +2,7 @@ var isFiltered;
 
 Template.linksList.helpers({
   links: function() {
-  	if (isFiltered == 'true') {
-  		console.log('yes filtering');
-  		//do nothing, because we are setting the links value in the click event
-  	} else {
+  	if (isFiltered != 'true') {
   		//initial load of all of the links
   		Session.set('links', Links.find().fetch()); 		
   	}
@@ -25,23 +22,16 @@ Template.linksList.events({
 	}
 });
 
-// buildArrayOfTags = function(){
-// 	Links.find()
-// }
-
-
 filterByTag = function(val){
 
 	if(typeof val.title === 'string'){
 		tagTitles = val.title;
-
 		Session.set('links', Links.find({ tags: { $elemMatch : {title: tagTitles}}}).fetch());
 		isFiltered = 'true';
 	} else {
 		Session.set('links', Links.find().fetch());
 	}	
 	Tracker.autorun(function() {
-		 //tell the helper that we are filtering, so it doesnt set the session collection
 		Session.get('links');
 	});
 }
