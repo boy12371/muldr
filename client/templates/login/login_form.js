4a1572a3d2228366d15ec52d0f4f630985ed4992
alexharris <alexharris6@gmail.com>
2014-12-17 21:50:09 -0800

    cleanup login_form.js comments

--- client/templates/login/login_form.js
+++ client/templates/login/login_form.js
@@ -2,28 +2,22 @@
 
     'submit #login-form' : function(e, t){
       e.preventDefault();
-      // retrieve the input field values
+
       var email = t.find('#login-username').value
         , password = t.find('#login-password').value;
 
-        // Trim and validate your fields here.... 
-
-        // If validation passes, supply the appropriate fields to the
-        // Meteor.loginWithPassword() function.
-        Meteor.loginWithPassword(email, password, function(err){
-        if (err) {
-          // The user might not have been found, or their passwword
-          // could be incorrect. Inform the user that their
-          // login attempt has failed. 	        	
+      Meteor.loginWithPassword(email, password, function(err){
+        if (err) {      	
         	console.log(err.reason)
         }
         else {
-          // The user has been logged in.
-          console.log('success');
+          console.log('successfully logged in');
         }
       });
-         return false; 
-      },
+      
+      return false; 
+    },
+
     'click #logout-button' : function(e, t){
     	Meteor.logout();
     }
