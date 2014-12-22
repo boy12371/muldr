f6c818457f44e3803d275b6de95f0d8de6e5d7ea
alexharris <alexharris6@gmail.com>
2014-12-22 10:52:48 -0800

    login form validation and errors

--- client/templates/login/login_form.js
+++ client/templates/login/login_form.js
@@ -1,24 +1,73 @@
  Template.loginForm.events({
 
-    'submit #login-form' : function(e, t){
-      e.preventDefault();
+  'submit #login-form' : function(e, t){
+    console.log('submitted');
+    e.preventDefault();
 
-      var email = t.find('#login-username').value
-        , password = t.find('#login-password').value;
+    var email = t.find('#login-username').value;
+    var password = t.find('#login-password').value;
 
-      Meteor.loginWithPassword(email, password, function(err){
-        if (err) {      	
-        	console.log(err.reason)
+    Meteor.loginWithPassword(email, password, function(err){
+      if (err) {      	
+        console.log(err.reason);
+        Session.set("formErrors", err.reason);
+      }
+      else {
+        console.log('successfully logged in');
+      }
+    });      
+    return false; 
+  },
+
+  'click #logout-button' : function(e, t){
+  	Meteor.logout();
+  }
+});
+
+ Template.loginForm.helpers({
+    formErrors: function() {
+      var formErrors = Session.get("formErrors");
+      return formErrors;
+  }
+ });
+
+ Template.loginForm.rendered = function(){
+
+  $('.ui #login-form').form({
+    username: {
+      identifier: 'username',
+      rules: [
+        {
+          type: 'empty',
+          prompt: 'Please enter your username'
         }
-        else {
-          console.log('successfully logged in');
-        }
-      });
-      
-      return false; 
+      ]
     },
-
-    'click #logout-button' : function(e, t){
-    	Meteor.logout();
-    }
-  });
+    password: {
+      identifier: 'password',
+      rules: [
+        {
+          type: 'empty',
+          prompt: 'Please enter your password'
+        },
+        {
+          type   : 'length[6]',
+          prompt : 'Your password must be at least 6 characters'
+        }
+      ]
+    },
+    errorMessage: {
+      identifier: 'error-message',
+      rules: [
+        {
+          type: 'is[User not found]',
+          prompt: 'User not found'
+        },
+        {
+          type: 'is[Incorrect Password]',
+          prompt: 'Password Incorrect'
+        }
+      ]
+    }  
+  })
+};
